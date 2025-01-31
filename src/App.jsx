4bdf8f0040690acf12cddc66f73d2358e8c2885b
nemusheep <miyomayo02@gmail.com>
2025-02-01 05:06:09 +0900

    cannot use parser-js

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+    const [bibJson, setBibJson] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        async function fetchBibtex() {
+            try {
+                const bibtexString = await fetch('/liter.bib');
+                const text = await bibtexString.text();
+                const parsedJson = parseBibtex(text);
+                setBibJson(parsedJson);
+            } catch(error) {
+                console.log("error :", error);
+                setBibJson([
+                    {
+                      entryType: "error",
+                      citationKey: "fallback",
+                      title: "データを取得できませんでした",
+                      author: "N/A",
+                      year: "N/A"
+                    }
+                  ]);
+            }
+        }
+
+        fetchBibtex();
+    }, [])    
+
+    return (
+        <>
+            <div className='app-body'>
+                <p>paper manager</p>
+                <button onClick={() => setCount((count) => count + 1)}>{count}</button>
+                <div className='paper-card-container'>
+                    {bibJson.map((entry, index) => (
+                        <div className='paper-card' key={index}>
+                            <h2>{entry.title}</h2>
+                            <p>{entry.author}</p>
+                            <p>{entry.year}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
---
+++ src/bibtex-parser-js.d.ts
@@ -0,0 +1,3 @@
+declare module "bibtex-parser-js" {
+    export function parse(bibtex: string): any;
+  }  
